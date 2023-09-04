971ce6b1e121cde9d696b1586e6e33b83b258f42
dholy <holyfrancisdushime@gmail.com>
2023-09-04 12:37:07 +0200

    country model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# country model.
+class Country(models.Model):
+    
+    name=models.CharField(max_length=250)
+    capital=models.CharField(max_length=200)
+    Iso2=models.CharField(max_length=20)
+    surface_are=models.DecimalField(max_digits=10, decimal_places=2)
+    gdp=models.IntegerField()
+    population=models.IntegerField()
+    pop_density=models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
+#
