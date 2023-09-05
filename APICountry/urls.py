4902ef7a78c10cdd63efcf87e97a303af37e4dbd
dholy <holyfrancisdushime@gmail.com>
2023-09-05 20:31:33 +0200

    url for validation

--- APICountry/urls.py
+++ APICountry/urls.py
@@ -8,15 +8,18 @@ from django.urls import include
 from rest_framework import routers
 from api.views import CountryViewset
 from api.views import CityViewset
+from api.views import PhoneNumberValidationViewset
 
 router = routers.SimpleRouter()
 
 router.register('country', CountryViewset, basename='country')
 router.register('city',CityViewset, basename='city')
+#router.register('validate',PhoneNumberValidationViewset, basename='validate')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('validate/', PhoneNumberValidationViewset.as_view(), name='phone_validation'),
     path('countries/',CountryViewset.country_list, name='countries'),
     path('country_details/',CountryViewset.country_details, name='country_details'),
     path('cities/',CityViewset.as_view({'get': 'list'}), name='cities'),
